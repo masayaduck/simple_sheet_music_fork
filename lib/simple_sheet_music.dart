36b79a2457b83693a0dffeb3abbc78ecd0fb1c9c
Masaya <136408105+masayaduck@users.noreply.github.com>
2025-07-24 23:09:56 +0900

    MusicalSymbolをimportできるように修正

--- lib/simple_sheet_music.dart
+++ lib/simple_sheet_music.dart
@@ -20,4 +20,4 @@ export 'src/music_objects/notes/single_note/note.dart' show Note;
 export 'src/music_objects/rest/rest.dart' show Rest;
 export 'src/music_objects/rest/rest_type.dart' show RestType;
 export 'src/music_objects/interface/musical_symbol.dart'
-    show MusicalSymbol, MusicalSymbolMetrics;
+    show MusicalSymbol;
